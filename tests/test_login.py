f5c0f2ae61ed353c24bb17316e8641a34f2d8a68
Dzmitry Sadouski <dmitrysadovskii@gmail.com>
2021-09-20 21:41:36 +0300

    add test to login with invalid data

--- tests/test_login.py
+++ tests/test_login.py
@@ -18,3 +18,19 @@ def test_login_to_the_system(browser):
         login_page.login("dmitry@gmail.com", "11111")
     with allure.step('Check account page displays'):
         AccountPage(browser).should_be_account_page()
+
+
+@allure.feature('Check user can not login to the system with invalid creds')
+def test_login_to_the_system(browser):
+    with allure.step('Open main page'):
+        main_page = MainPage(browser)
+        main_page.open_main_page()
+    with allure.step('Open login page'):
+        main_page.open_login_page()
+    with allure.step('Check login page displays'):
+        login_page = LoginPage(browser)
+        login_page.should_be_login_page()
+    with allure.step('Login user to the system'):
+        login_page.login("dmitry@gmail.com", "111112")
+    with allure.step('Check account page displays'):
+        AccountPage(browser).should_be_account_page()
