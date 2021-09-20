777ed87971cb5991d4bc909470780514f372172d
Dzmitry Sadouski <dmitrysadovskii@gmail.com>
2021-09-20 21:53:46 +0300

    add hook screenshot

--- conftest.py
+++ conftest.py
@@ -1,3 +1,4 @@
+import allure
 from selenium import webdriver
 import pytest
 import os
@@ -21,3 +22,19 @@ def pytest_configure(config):
     if not os.path.exists('html_reports'):
         os.makedirs('html_reports')
     config.option.htmlpath = f'html_reports/ {datetime.now().strftime("%d-%m-%Y %H-%M-%S")}.html'
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item):
+    outcome = yield
+    rep = outcome.get_result()
+    if rep.failed:
+        try:
+            if 'browser' in item.fixturenames:
+                browser = item.funcargs['browser']
+            else:
+                print('Does not have browser fixture')
+                return
+            allure.attach(browser.get_screenshot_as_png(), "Screenshot", attachment_type=allure.attachment_type.PNG)
+        except Exception as e:
+            print(f'Failed to make screenshot: {e}')
