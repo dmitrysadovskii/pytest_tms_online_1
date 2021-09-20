954debaa63f9563e5f7bfbe873c5e43a28cad1bc
Dzmitry Sadouski <dmitrysadovskii@gmail.com>
2021-09-20 20:40:45 +0300

    add headless mode for chrome browser

--- conftest.py
+++ conftest.py
@@ -6,7 +6,11 @@ from datetime import datetime
 
 @pytest.fixture()
 def browser():
-    browser = webdriver.Chrome()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    browser = webdriver.Chrome(chrome_options=chrome_options)
     browser.maximize_window()
     yield browser
     browser.quit()
